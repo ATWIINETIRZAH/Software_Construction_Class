c70726f5ee25f92d2e94b1389f7cb87c6711564f
ATWIINETIRZAH <tirzahatwiine5@gmail.com>
2025-02-18 15:30:33 +0300

    updated users/models.py file

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,12 +1,19 @@
-
-
+from django.contrib.auth.models import AbstractUser , Group, Permission
 from django.db import models
 
-# Create your models here.
-from django.contrib.auth.models import AbstractUser
-from django.db import models
 
-class CustomUser(AbstractUser):
-    email = models. EmailField(unique=True)
+class CustomerUser(AbstractUser):
+    email = models.EmailField(unique = True)
 
+    groups = models.ManyToManyField(
+        Group,
+        related_name="customuser_groups",  # Change related_name to avoid clash
+        blank=True
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="customuser_permissions",  # Change related_name to avoid clash
+        blank=True
+    )
     
