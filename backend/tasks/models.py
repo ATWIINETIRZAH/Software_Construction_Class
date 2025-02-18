4902f45214cff4263190b888fdad0f82b7bac9ef
ATWIINETIRZAH <tirzahatwiine5@gmail.com>
2025-02-18 22:50:26 +0300

    fix: fixed circular import and added appropriate on_delete behaviour oin tasks folder

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,11 +1,20 @@
-from django.db import models
+
+
+# # Create your models here.
+
+# from django.db import models
+# from users.models import CustomUser
+
+# class Task(models.Model):
+#     title = models. CharField(max_length=255)
+#     description = models. TextField()
+#     assigned_to = models. ForeignKey(CustomUser, on_delete=models)
 
 # Create your models here.
 
 from django.db import models
-from users.models import CustomUser
 
 class Task(models.Model):
-    title = models. CharField(max_length=255)
-    description = models. TextField()
-    assigned_to = models. ForeignKey(CustomUser, on_delete=models)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    assigned_to = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)  # Fix circular import and add appropriate on_delete behavior
