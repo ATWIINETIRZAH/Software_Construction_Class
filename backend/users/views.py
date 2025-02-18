67677517e83ee6fdc69e40af0165ff9df5b8a124
ATWIINETIRZAH <tirzahatwiine5@gmail.com>
2025-02-18 23:07:16 +0300

    feat:created API Endpoints

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import CustomUser
+from .serializer import UserSerializer
+
+class UserListCreate(generics.ListCreateAPIView):
+    queryset = CustomUser. objects. all()
+    serializer_class = UserSerializer
