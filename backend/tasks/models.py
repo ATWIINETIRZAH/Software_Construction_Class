410c47c738a29fd1d7ed8a9216930ab7470c3f63
ATWIINETIRZAH <tirzahatwiine5@gmail.com>
2025-03-02 22:34:36 +0300

    feat: added created_at field to class Task🏢

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -12,9 +12,21 @@
 
 # Create your models here.
 
+# from django.db import models
+
+# class Task(models.Model):
+#     title = models.CharField(max_length=255)
+#     description = models.TextField()
+#     assigned_to = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)  # Fix circular import and add appropriate on_delete behavior
+
 from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    assigned_to = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)  # Fix circular import and add appropriate on_delete behavior
+    assigned_to = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)  # Remove default
+
+    def __str__(self):
+        return self.title
+
