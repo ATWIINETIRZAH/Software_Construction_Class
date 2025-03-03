682d73f2298deebf4e7e5bae7bdb91704b21af49
ATWIINETIRZAH <tirzahatwiine5@gmail.com>
2025-03-03 22:50:12 +0300

    feat: added userDetailView class

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+# # Create your views here.
+# from rest_framework import generics
+# from .models import CustomUser
+# from .serializer import UserSerializer
+
+# class UserListCreate(generics.ListCreateAPIView):
+#     queryset = CustomUser. objects. all()
+#     serializer_class = UserSerializer
+
 from rest_framework import generics
 from .models import CustomUser
 from .serializer import UserSerializer
 
 class UserListCreate(generics.ListCreateAPIView):
-    queryset = CustomUser. objects. all()
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
